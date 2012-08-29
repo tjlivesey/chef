a4d2ce0dfb2a4a21b957587f03f5f8cefd22bf1f
Tom Livesey <tjlivesey@gmail.com>
2012-08-29 17:22:18 +0100

    create deploy user before installing nginx

--- roles/web.rb
+++ roles/web.rb
@@ -1,4 +1,4 @@
 name "web"
 description "The base role for systems that serve HTTP traffic"
-run_list "recipe[nginx]", "recipe[user::data_bag]"
+run_list "recipe[user::data_bag]", "recipe[nginx]"
 default_attributes :users => ["deploy"]
