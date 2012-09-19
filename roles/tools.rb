6471fa6ee7591d5b79ad883c5727ed52721dc5b0
Tom Livesey <tjlivesey@gmail.com>
2012-09-19 16:08:56 +0100

    add standard nginx package to tools

--- roles/tools.rb
+++ roles/tools.rb
@@ -1,4 +1,4 @@
 name "tools"
 description "admin tools as well as droplet dashboard"
-run_list "recipe[user::data_bag]", "recipe[nginx::passenger]"
+run_list "recipe[user::data_bag]", "recipe[nginx]", "recipe[nginx::passenger]"
 default_attributes :users => ["deploy"]
